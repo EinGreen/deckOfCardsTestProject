7a609881fd39c5233fc50ae273e09d87f0c3311c
Ein_Green <mbananola2002@gmail.com>
2021-04-03 00:02:18 -0700

    added a draw card function with appropriate button, edited the readMe page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
+let ajax = new XMLHttpRequest();
 function shuffle(e) {
-    let ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
             let deckSetup = JSON.parse(this.responseText);
+            let deckId = deckSetup.deck_id;
+            let remainingCards = deckSetup.remaining;
+            console.log(`Deck ID: ${deckId}`);
+            document.getElementById(`deckId`).innerText = `Deck ID: ${deckId}`;
+            document.getElementById(`remainingCardsCounter`).innerText = `Remaining Cards: ${remainingCards}`;
+            Cookies.set(`deckIdentification`, deckId);
             if (deckSetup.success) {
                 document.getElementById(`shuffleMessage`).innerText = `The Deck has been Shuffled`
             } else if (!deckSetup.success) {
@@ -16,4 +22,22 @@ function shuffle(e) {
 }
 
 let shuffleButton = document.getElementById(`shuffleButton`);
-shuffleButton.addEventListener("click", shuffle)
+shuffleButton.addEventListener("click", shuffle);
+
+let newDeckId = Cookies.get(`deckIdentification`);
+function draw(e) {
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            let drawSetup = JSON.parse(this.responseText);
+            let remainingCards = drawSetup.remaining;
+            document.getElementById(`remainingCardsCounter`).innerText = `Remaining Cards: ${remainingCards}`;
+        }
+    } 
+    ajax.open("GET", `https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=1`, true);
+    ajax.send();
+}
+
+let drawButton = document.getElementById(`drawCardButton`);
+drawButton.addEventListener(`click`, draw)
--- readMe.md
+++ readMe.md
@@ -2,3 +2,8 @@ Deck of Cards API from: https://github.com/public-apis/public-apis#games--comics
 Just freaking look at it
 
 Shuffle API: https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+<!-- Note: you can set the number of decks by manipulating the deck_count number -->
+
+Draw API: https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+<!-- Note: you can set the number of cards drawn by manipulating the count number -->
+<!-- Note2: make sure you get the deck Id before drawing a card -->
